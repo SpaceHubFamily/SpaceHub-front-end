e435ed336e5673d95ce0947b54a038b78ea2e0fd
Tom Krepp <tommykrepp@aol.com>
2017-05-01 15:02:03 -0600

    App hooked up

--- public/index.js
+++ public/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.js'
+import routes from './routes.js';
 
 require('./styles/main.css');
 
 ReactDOM.render(
   <App />,
-  document.getElementById('#app')
+  document.getElementById('app')
 );

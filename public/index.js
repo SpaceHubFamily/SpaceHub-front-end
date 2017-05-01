ec20e1016287b2fc76d541267f728e0d0ab2d68d
DariaDesiderata <daria.calwell@gmail.com>
2017-05-01 12:18:25 -0600

    add material-ui

---
+++ public/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'

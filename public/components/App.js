0c96d6d6633c34272a75e33a9960355b2727d6b3
NickSugar <nicksugar1@outlook.com>
2017-05-02 20:01:22 -0600

    got a basic redux

--- public/components/App.js
+++ public/components/App.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { Switch, Route } from 'react-router';
+import {Provider} from 'react-redux';
 import LandingPage from './LandingPage';
 import CompanyPage from './CompanyPage';
 import UserPage from './UserPage';
-import SetUpPage from './containers/SetUpPage';
-import { Switch, Route } from 'react-router';
+import SetUpPage from '../containers/SetUpPage';
 
 
 injectTapEventPlugin();
@@ -46,6 +47,13 @@ const muiTheme = getMuiTheme({
 });
 
 
+// var actions = require('actions');
+var setUpStore = require('../stores/setUpStore.js').configure();
+
+setUpStore.subscribe(() => {
+  console.log('New state', setUpStore.getState());
+});
+
 const App = () => (
   <MuiThemeProvider muiTheme={muiTheme}>
       <div>
@@ -53,7 +61,9 @@ const App = () => (
             <Route exact path="/" component={LandingPage} />
             <Route path="/company" component={CompanyPage} />
             <Route path="/user" component={UserPage} />
-            <Route path="/setup" component={SetUpPage} />
+            <Provider store={setUpStore}>
+              <Route path="/setup" component={SetUpPage} />
+            </Provider>
         </Switch>
       </div>
   </MuiThemeProvider>

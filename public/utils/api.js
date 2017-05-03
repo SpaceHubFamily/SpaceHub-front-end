1b3ef0c93f162b9fd3885dc98c02c268b9933de6
DariaDesiderata <daria.calwell@gmail.com>
2017-05-03 10:00:10 -0600

    add pendig rooms

--- public/utils/api.js
+++ public/utils/api.js
@@ -4,9 +4,13 @@ const roomApis = {
 
   getOpenRooms: function() {
     return axios.get('https://spacehubapi.herokuapp.com/room/user/4')
-      .then(response => response.data.filter(item => item.event_planner === true) )
-  }
+      .then(response => response.data.filter(item => item.room_available === true) )
+  },
 
+  getPendingRooms: function() {
+    return axios.get('https://spacehubapi.herokuapp.com/shindig/user/4')
+      .then(response => console.log(response.data))
+  }
 }
 
 export default roomApis

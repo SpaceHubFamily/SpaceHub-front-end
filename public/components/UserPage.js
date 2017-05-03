c6b8691a2cdfa5f33b3c2588f5cd8d33e34b5479
Tom Krepp <tommykrepp@aol.com>
2017-05-02 21:43:34 -0600

    User Page Started

--- public/components/UserPage.js
+++ public/components/UserPage.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import ReactDOM from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import SideNav from './SideNav.js'
+import SearchBar from './search_bar.js'
+
 
 const UserPage = () => (
-  <RaisedButton label="UserPage" />
+  <MuiThemeProvider>
+    <div>
+    <SideNav />
+  </div>
+  </MuiThemeProvider>
 );
 
 export default UserPage;

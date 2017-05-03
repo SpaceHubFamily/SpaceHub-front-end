bb65f4161c56bd50337e76a2e72a92bdde975354
NickSugar <nicksugar1@outlook.com>
2017-05-03 12:05:03 -0600

    testing state

--- public/stores/setUpStore.js
+++ public/stores/setUpStore.js
@@ -3,8 +3,29 @@ var redux = require('redux')
 import {provideSpaceReducer} from '../reducers/setUpReducers.js'
 
 var initialState = {
-  'venue_owner': false
+  'test': true
+  // 'venue_owner': false,
+  // 'newVenueForm': {
+  //   address: '',
+  //   city: ''
+  // }
 }
+
+console.log(initialState);
+
+// table.increments();
+// table.string('address');
+// table.string('city');
+// table.string('state');
+// table.integer('zip_code');
+// table.string('name');
+// table.string('email');
+// table.string('phone');
+// table.string('description', [1000]);
+// table.string('img_url');
+// table.integer('user_id')
+//   .references('users.id')
+//   .onDelete('CASCADE');
 
 export var configure = (initialState) => {
   var reducer = redux.combineReducers({

f3281cc822a3c18a2340357e4ed9d938b5682838
NickSugar <nicksugar1@outlook.com>
2017-05-04 22:57:07 -0600

    shindig form works

--- public/components/company/eventSetter.js
+++ public/components/company/eventSetter.js
@@ -1,34 +1,69 @@
-import React from 'react';
+var React = require('react');
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import DatePicker from 'material-ui/DatePicker';
 import TimePicker from 'material-ui/TimePicker';
 var {connect} = require('react-redux');
+import axios from 'axios'
 
 /**
  * Dialogs can be nested. This example opens a Date Picker from within a Dialog.
  */
 class EventSetter extends React.Component {
 
-  state = {
-    open: false,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: null,
+      start_time: null,
+      end_time: null,
+      open: false
+    };
+  }
+
+  handleChangeDate = (event, date) => {
+    this.setState({
+      date: date
+    })
+  }
+
+  handleChangeStartTime = (event, start_time) => {
+    this.setState({
+      start_time: start_time
+    })
+  }
+
+  handleChangeEndTime = (event, end_time) => {
+    this.setState({
+      end_time: end_time
+    })
+  }
 
   handleOpen = () => {
-    this.setState({open: true});
-  };
+    this.setState({open: true})
+  }
 
   handleSubmitClose = () => {
-    this.setState({open: false});
+    this.setState({open: false})
 
-    axios.post('https://spacehubapi.herokuapp.com/venue', newVenue)
-    .then(res => console.log(res, 'good'))
-    .catch(err => console.log(err, 'bad'))
-  };
+    var shindig = {}
+
+    shindig.date = this.state.date
+    shindig.start_time = this.state.start_time
+    shindig.end_time = this.state.end_time
+    shindig.available = true
+    shindig.user_id = 1
+    shindig.room_id = this.props.roomID
+
+    axios.post('https://spacehubapi.herokuapp.com/shindig', shindig)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
 
   render() {
-    console.log(this.props.roomID);
+
     var style = {
       imput: {
         display: 'inline'
@@ -56,12 +91,18 @@ class EventSetter extends React.Component {
 
           <DatePicker
             style={style.imput}
+            value={this.state.date}
+            onChange={this.handleChangeDate}
             hintText="Date" />
           <TimePicker
             style={style.imput}
+            value={this.state.start_time}
+            onChange={this.handleChangeStartTime}
             hintText="Start Time" />
           <TimePicker
             style={style.imput}
+            value={this.state.end_time}
+            onChange={this.handleChangeEndTime}
             hintText="End Time" />
         </Dialog>
       </div>
